fcf60b9fb570e042f84da6010712ae3fcc6063f5
Jason Loo <loo.jason95@gmail.com>
2016-01-11 15:59:58 -0500

    Rails and Ruby update

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,13 @@ require 'json'
 character_data_file = 'app/data/characters.json'
 file = File.read(character_data_file)
 characters = JSON.parse(file)
-puts "Inputting character data"
+puts "Inputting character data."
 characters.each do |character|
-	Character.create!({ name: character["name"], 
-						id: character["id"],
-						imagename: character["imagename"],
-						colour: character["colour"] })
-	puts "#{character["name"]} added"
+	Character.create!({
+    name: character["name"],
+		id: character["id"],
+	  imagename: character["imagename"],
+		colour: character["colour"]
+  })
+  puts "#{character["name"]} added."
 end
